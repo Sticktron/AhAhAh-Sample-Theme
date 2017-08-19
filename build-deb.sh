c23417cc66783c1a406bf0f33b661e02168f5bef
Sticktron <sticktron@hotmail.com>
2017-08-19 16:53:18 -0400

    fixed build script (wrong compression type)

--- build-deb.sh
+++ build-deb.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 folder="Sample Theme"
-package="com.sticktron.ahahah-sample-theme"
+package="com.sticktron.ahahah-sample-theme_1.1"
 
 echo ""
 echo "1) Cleaning up..."
 
 # get rid of these pesky buggers
-find "$folder/" -name ".DS_Store" -delete
+find "$folder" -name ".DS_Store" -delete
 
 
 echo ""
-echo "2) Packaging Theme ..."
+echo "2) Packaging ..."
 
 # build the deb file from the "Sample Package" folder
-dpkg-deb -b "$folder"
+dpkg-deb -Zgzip -b "$folder"
 
 
 echo ""
-echo "3) Renaming '$folder.deb' to '$package.deb' ..."
+echo "3) Renaming ..."
 
 # rename the deb file to match the package name
-mv "$folder.deb" "$package.deb"
+mv -v "$folder.deb" "$package.deb"
 
 
 echo ""
